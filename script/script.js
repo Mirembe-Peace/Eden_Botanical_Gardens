a61a357761c78c9f327bf4fdae061b0a5f6ff684
Mirembe Peace Mercy <peace2de@gmail.com>
2025-08-18 21:25:43 +0300

    some changes

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ const height = canvas.clientHeight;
 const scene = new THREE.Scene();
 
 //camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1,1000);
 camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
 scene.add(camera);
